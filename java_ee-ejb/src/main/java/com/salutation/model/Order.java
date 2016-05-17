d4358b39e9e89a055b46143a5e60d0da826ffbf0
khayapro <kmathebula@fnb.co.za>
2016-05-17 16:57:52 +0200

    order bean...

--- java_ee-ejb/src/main/java/com/salutation/model/Order.java
+++ java_ee-ejb/src/main/java/com/salutation/model/Order.java
@@ -1,7 +1,37 @@
 package com.salutation.model;
 
+import java.io.Serializable;
+
 /**
  * Created by khayapro on 2016/05/17
  */
-public class Order {
+public class Order implements Serializable {
+
+    private int partNumber;
+    private float weight;
+    private int quantity;
+
+    /**
+     * Constructor for order
+     * @param partNumber
+     * @param weight
+     * @param quantity
+     */
+    public Order(final int partNumber, final float weight, final int quantity){
+        this.partNumber = partNumber;
+        this.weight = weight;
+        this.quantity = quantity;
+    }
+
+    public int getPartNumber() {
+        return partNumber;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
