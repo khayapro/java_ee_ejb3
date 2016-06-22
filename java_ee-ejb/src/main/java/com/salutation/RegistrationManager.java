e215d55ea3995456c3906ee33f00dd8b1e27b8e8
khayapro <kmathebula@fnb.co.za>
2016-06-22 18:49:44 +0200

    demonstrating internal level interceptor for order of invocationg, i.e External, default, class, method & internal

--- java_ee-ejb/src/main/java/com/salutation/RegistrationManager.java
+++ java_ee-ejb/src/main/java/com/salutation/RegistrationManager.java
@@ -6,8 +6,10 @@ import com.salutation.model.Attendee;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateful;
+import javax.interceptor.AroundInvoke;
 import javax.interceptor.ExcludeClassInterceptors;
 import javax.interceptor.Interceptors;
+import javax.interceptor.InvocationContext;
 
 /**
  * Created by khayapro on 2016/06/21
@@ -39,6 +41,20 @@ public class RegistrationManager {
         attendeeFacade.save(attendee);
         System.out.println(name + " registered");
         return attendee;
+    }
+
+    /**
+     * This is a class level interceptor declaring demo
+     * @param context
+     * @return
+     * @throws Exception
+     */
+    @AroundInvoke
+    public Object internalInterceptor(InvocationContext context) throws Exception {
+        System.out.println("RegistrationManager - internal method invoking : " + context.getMethod().getName());
+        final Object result = context.proceed(); //proceed to targeted method.
+        System.out.println("RegistrationManager - internal method invoking : " + context.getMethod().getName());
+        return result;
     }
 
 
